896194c59beb6ecd7783acbd3c395f683aebccfb
Osmtj001 <osmtj001@mymail.unisa.edu.au>
2025-11-18 11:39:00 +1030

    Initial Commit

--- enclosure.py
+++ enclosure.py
@@ -1,8 +1,8 @@
 '''
-File: filename.py
+File: enclosure.py
 Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+Author: Trent Osmond
+ID: 110316757
+Username: Osmtj001
 This is my own work as defined by the University's Academic Integrity Policy.
 ''
