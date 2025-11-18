bb22eb71fe61b6d6fc23ae49e679bdac14075fee
Osmtj001 <osmtj001@mymail.unisa.edu.au>
2025-11-18 12:06:56 +1030

    Added dummy test & further functionality to add animals to project

--- animal.py
+++ animal.py
@@ -14,6 +14,8 @@ This is my own work as defined by the University's Academic Integrity Policy.
 # making sounds, ea�ng, and sleeping.
 
 class Animal:
+
+    Categories = ["Bird"]
     def __init__(self, name, species, age, diet, category, sound):
         self.name = name
         self.species = species
@@ -21,6 +23,18 @@ class Animal:
         self.diet = diet
         self.category = category
         self.sound = sound
+
+    def add_animal(self):
+        A_name = input("Please enter animal Name")
+        A_Species = input("Please enter animal Species")
+        A_Age = input("Please enter animal Age")
+        A_Diet = input("Please enter animal Diet")
+        A_Category = ""
+        while A_Category not in Animal.Categories:
+            A_Category = input(f"Please enter animal Category from {Animal.Categories}")
+        A_Sound = input("Please enter animal Sound")
+
+        return Animal(A_name, A_Species, A_Age, A_Diet, A_Category, A_Sound)
 
     def make_sound(self):
         print(self.sound)
@@ -31,7 +45,12 @@ class Animal:
     def sleep(self):
         print(f"{self.name} is now sleeping. ZZZzzz.")
 
-Parrot = Animal("Parrot", "bird", 12, "worms", "bird", "Cacaw")
-Parrot.make_sound()
-Parrot.sleep()
-Parrot.eat_food()
+dummy = Animal("", "", 0, "", "", "")
+new_animal = dummy.add_animal()
+
+print(new_animal.name)
+new_animal.make_sound()
+# Parrot = Animal("Parrot", "bird", 12, "worms", "bird", "Cacaw")
+# Parrot.make_sound()
+# Parrot.sleep()
+# Parrot.eat_food()
