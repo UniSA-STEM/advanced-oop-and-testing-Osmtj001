a1e54d4184367de6d27ae08b916f7e14ff2d89aa
Osmtj001 <osmtj001@mymail.unisa.edu.au>
2025-11-21 22:13:25 +1030

    Added basic getters & setters for name & size, along with property which will be used later on in project

--- enclosure.py
+++ enclosure.py
@@ -7,16 +7,39 @@ Username: Osmtj001
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
 
-class Emclosure:
+class Enclosure:
     def __init__(self, name, size, environment, animal_type):
-        self.name = name
-        self.size = size
-        self.environment = environment
-        self.animal_type = animal_type
+        self.__name = name
+        self.__size = size
+        self.__environment = environment
+        self.__animal_type = animal_type
+        self.__clean = 100
+        self.__animal = []
 
+    def get_name(self):
+        return self.__name
 
+    def set_name(self, name):
+        if isinstance(name, str) and name is not None:
+            self.__name = name
+            print(f"Enclosure name: {name}")
+        else:
+            self.__name = "Default Enclosure"
+            print(f"Enclosure name did not meet requirements: {self.__name} has been assigned")
+    name = property(get_name, set_name)
 
+    def get_size(self):
+        return self.__size
 
+    def set_size(self, size):
+        if isinstance(size, int) and size is not None:
+            self.__size = size
+            print(f"Enclosure size: {size}")
+        else:
+            self.__size = "0"
+            print(f"Enclosure size did not meet requirements. Size has been defaulted to {self.__size}")
+
+    size = property(get_size, set_size)
 
 # Enclosures are used to house animals and must include proper�es such as size,
 # environmental type (e.g., aqua�c, savannah), and cleanliness level. Each enclosure is
