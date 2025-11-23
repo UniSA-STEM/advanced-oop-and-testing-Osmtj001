addb358829b85c7c6d5e36c6ecb9553c3e2f3b19
Osmtj001 <osmtj001@mymail.unisa.edu.au>
2025-11-23 16:06:49 +1030

    Added health class which takes values of description, date reported, severity level & treatment plan to output animal medical record. This function validates all provided data & will allow a boolean value for if the treatment is active, or resolved. Adding this class also involved adjusting the Animal class to allow for a list of health issues for each animal

--- animal.py
+++ animal.py
@@ -23,6 +23,7 @@ class Animal:
         self.diet = diet
         self.category = category
         self.sound = sound
+        self.__health_issues = []
 
     # def add_animal(self):
     #     A_name = input("Please enter animal Name")
@@ -54,3 +55,75 @@ class Animal:
 # # Parrot.make_sound()
 # # Parrot.sleep()
 # # Parrot.eat_food()
+
+class Health:
+    def __init__(self, description, date_reported, level, treatment_plan):
+        self.description = description
+        self.date_reported = date_reported
+        self.level = level
+        self.treatment_plan = treatment_plan
+        self.active = True
+
+    def __get_description(self):
+        return self.__description
+    def __set_description(self, description):
+        if isinstance(description, str):
+            self.__description = description
+        else:
+            self.__description = "Default Description"
+            print(f"Invalid Description: {self.__description} has been set")
+
+    def __get_date_reported(self):
+        return self.__date_reported
+
+    def __set_date_reported(self, date_reported):
+        if isinstance(date_reported, str):
+            self.__date_reported = date_reported
+        else:
+            self.__date_report = "Unknown"
+            print(f"Invalid Date Provided: {self.__date_report} has been set")
+
+    def __get_level(self):
+        return self.__level
+
+    def __set_level(self, level):
+        if isinstance(level, str):
+            self.__level = level
+        else:
+            self.__level = "Unknown"
+            print(f"Invalid Level of Severity Provided {self.__level} has been set")
+
+    def __get_treatment_plan(self):
+        return self.__treatment_plan
+    def __set_treatment_plan(self, treatment_plan):
+        if isinstance(treatment_plan, str):
+            self.__treatment_plan = treatment_plan
+        else:
+            self.__treatment_plan = "Unknown"
+            print(f"Invalid Treatment Plan: {self.__treatment_plan} has been set")
+
+    def __get_active(self):
+        return self.__get_active()
+    def __set_active(self, active):
+        if isinstance(active, bool):
+            self.__active = active
+        else:
+            self.__active = True
+            print(f"Invalid Active Value: {self.__active} has been set for patient illness value")
+
+    def close_issue(self):
+        self.active = False
+
+    def __str__(self):
+        active_status = "Active" if self.active else "Resolved"
+        return(f"Health Issue: {self.description},"
+               f"Date Reported: {self.__date_report},"
+               f"Level of Severity: {self.__level},"
+               f"Treatment Plan: {self.__treatment_plan},"
+               f"Active Status: {active_status}")
+
+    description = property(__get_description, __set_description)
+    date = property(__get_date_reported, __set_date_reported)
+    level = property(__get_level, __set_level)
+    treatment = property(__get_treatment_plan, __set_treatment_plan)
+    active = property(__get_active, __set_active)
